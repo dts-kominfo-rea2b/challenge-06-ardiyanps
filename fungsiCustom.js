3ba6f81cc2e34aa66204098b16d65534a0c8b073
Ardiyan puji santoso <40789455+ardiyanps@users.noreply.github.com>
2022-06-09 22:27:40 +0700

    Bismillah merchandise

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,53 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData1 = (nextStep) => {
+  fs.readFile(file1, 'utf-8', (err, data) => {
+    if (err) {
+      return console.log(err);  
+    }
+      let isiData1 = JSON.parse(data).message.slice(5);
+      let data1 = [];
+      data1.push(isiData1);
+      nextStep(data1);
+  })};
+  const bacaData2 = (data1, nextStep) => {
+      fs.readFile(file2, 'utf-8', (err, data) => {
+        if (err) {
+          return console.log(err);  
+        }
+          let isiData2 = JSON.parse(data)[0].message.slice(5);
+          let data2 = data1;
+          data2.push(isiData2);
+          nextStep(data2);
+          
+      })};
+  const bacaData3 = (data2, nextStep) => {
+      fs.readFile(file3, 'utf-8', (err, data) => {
+        if (err) {
+          return console.log(err);  
+        }
+          let isiData3 = JSON.parse(data)[0].data.message.slice(5);
+          let data3 = data2;
+          data3.push(isiData3);
+          nextStep(data3);
+      })};
+  const fnCallback = (err, data) => {
+      if (err) {
+          return console.log(err);  
+        }
+      return data;
+  }
+const bacaData = (fnCallback) => {
+      bacaData1((data1) => {
+          bacaData2(data1, (data2) => {
+              bacaData3(data2, (data3) => {
+                  fnCallback(data3);
+              });
+          });
+      });
+}
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
